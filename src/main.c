e8ba738fc004aea481c16add21678121fe7e3af1
an94dy <an94dy@users.noreply.github.com>
2015-02-28 21:17:57 +0000

    Added simple walls

--- src/main.c
+++ src/main.c
@@ -5,7 +5,11 @@
 #include <pebble.h>
 #include "pge.h"
   
+#define MARGIN 3
+#define WALL_WIDTH 8
 static Window *s_game_window;
+
+int currx, curry, boundx, boundy;
 
   
 GPoint createPoint(int a, int b)
@@ -25,7 +29,9 @@ static void game_draw(GContext *ctx) {
   
   graphics_context_set_stroke_color(ctx, GColorFromRGB(255, 0, 0));
   graphics_context_set_fill_color(ctx, GColorFromRGB(255, 0, 0));
-  graphics_draw_line(ctx, createPoint(2,3), createPoint(70,80));
+
+  graphics_fill_rect(ctx, GRect(MARGIN, MARGIN, WALL_WIDTH, boundy - 2 * MARGIN), 0, 0);
+  graphics_fill_rect(ctx, GRect(boundx - MARGIN - WALL_WIDTH, MARGIN, WALL_WIDTH, boundy - 2 * MARGIN), 0, 0);
 }
 
 static void game_click(int button_id, bool long_click) {
@@ -36,6 +42,16 @@ static void game_click(int button_id, bool long_click) {
 void pge_init() {
   // Start the game, keep a Window reference for later
   s_game_window = pge_begin(GColorBlack, game_logic, game_draw, game_click);
+  
+  Layer *window_layer = window_get_root_layer(s_game_window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  boundx = bounds.size.w;
+  boundy = bounds.size.h;
+  
+  currx = 0;
+  curry = boundy + 4;
+  
 }
 
 
